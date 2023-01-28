e4ac73bec5e2d427c0068086ab54d8f19e24b9a7
mauroapjr <mauroapjr@outlook.com>
2023-01-27 20:13:00 -0500

    create file for functions

---
+++ helpers.js
@@ -0,0 +1,17 @@
+
+const getUserByEmail = (emailToFind, usersDatabase) => {
+  for (const userKey in usersDatabase) {
+    console.log("userKey:", userKey);
+    //object.key//this is I grab the value of an object
+    //object[key]//this is I grab the value of an object
+    const currentUser = usersDatabase[userKey]; // this gets the user values
+    console.log("CURRENT USER:", currentUser);
+    console.log("USER EMAIL TO FIND:", emailToFind);
+    if (currentUser.email === emailToFind) {
+      return currentUser;
+    }
+  }
+  return undefined;
+};
+
+module.exports = { getUserByEmail };
